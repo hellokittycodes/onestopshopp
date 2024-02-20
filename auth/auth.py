3dbfe2041620c03fbb645d41894330e2f6f1efe6
allergic2everything <ritika.myana@gmail.com>
2024-02-20 12:25:46 -0500

    commit msg

---
+++ auth/auth.py
@@ -0,0 +1,11 @@
+from flask import Blueprint 
+
+#haha i commented 
+
+#this file is a blueprint of our application 
+#to separate our pages out 
+#woooooo 
+
+auth = Blueprint('auth', __name__) 
+
+# we have now set up a blueprint for our flask app 
